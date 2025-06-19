7a1ba5b57cc84ce4f06a337edef0cb836f04bbc7
Prince_Samota <prince@cogitohub.com>
2025-06-20 03:57:49 +0530

    Ui fix and raw material inwords new changes added

--- app/controllers/raw_material_inwards_controller.rb
+++ app/controllers/raw_material_inwards_controller.rb
@@ -1,38 +1,26 @@
 # app/controllers/raw_material_inwards_controller.rb
 
 class RawMaterialInwardsController < ApplicationController
-    def new
-      @raw_material_inward = RawMaterialInward.new
-    end
+  def new
+    @raw_material_inward = RawMaterialInward.new
+    @raw_material_inwards = RawMaterialInward.all.order(created_at: :desc)
+  end
   
-    def index
-        @raw_material_inwards = RawMaterialInward.all.order(created_at: :desc)
+  def create
+    @raw_material_inward = RawMaterialInward.new(raw_material_inward_params)
+    if @raw_material_inward.save
+      redirect_to new_raw_material_inward_path, notice: "Raw material inward created successfully."
+    else
+      @raw_material_inwards = RawMaterialInward.all.order(created_at: :desc)
+      render :new, status: :unprocessable_entity
     end
-
-    def create
-      @raw_material_inward = RawMaterialInward.new(raw_material_inward_params)
-      if @raw_material_inward.save
-        redirect_to raw_material_inwards_path, notice: 'Raw material inward successfully created.'
-      else
-        render :new
-      end
-    end
+  end
   
-    def show
-      @raw_material_inward = RawMaterialInward.find(params[:id])
-    end
+  private
   
-    private
+  def raw_material_inward_params
+    params.require(:raw_material_inward).permit(:supplier_name, :receiving_date, :sku_id, :item_name, :receiving_quantity, :purchase_price)
+  end
   
-    def raw_material_inward_params
-      params.require(:raw_material_inward).permit(
-        :supplier_name,
-        :receiving_date,
-        :sku_id,
-        :item_name,
-        :receiving_quantity,
-        :purchase_price
-      )
-    end
   end
   
