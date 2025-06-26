0654e04979466f4cc31a4ac1dabda8b4bea67a98
Prince_Samota <prince@cogitohub.com>
2025-06-26 14:56:58 +0530

    search added

--- app/controllers/bill_of_materials_controller.rb
+++ app/controllers/bill_of_materials_controller.rb
@@ -1,4 +1,5 @@
 class BillOfMaterialsController < ApplicationController
+  
   def new
     @bill_of_material = BillOfMaterial.new
     @bill_of_material.build_finished_good
@@ -19,6 +20,15 @@ class BillOfMaterialsController < ApplicationController
     @bill_of_materials = BillOfMaterial.includes(:finished_good, :bom_raw_material_items).order(created_at: :desc)
   end
 
+  def clone
+    original_bom = BillOfMaterial.find(params[:id])
+    @bill_of_material = original_bom.dup
+    @bill_of_material.finished_good = original_bom.finished_good.dup if original_bom.finished_good.present?
+    @bill_of_material.bom_raw_material_items = original_bom.bom_raw_material_items.map(&:dup)
+  
+    @item_masters = ItemMaster.all  # ✅ Ensure this is present
+    render :new
+  end
   private
 
   def bom_params
