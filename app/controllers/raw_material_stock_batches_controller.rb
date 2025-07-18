ec5f6f4e926dbd262689d5ef3b8b8fc13248c124
Prince_Samota <prince@cogitohub.com>
2025-07-18 09:36:03 +0530

    BOM bug fix

--- app/controllers/raw_material_stock_batches_controller.rb
+++ app/controllers/raw_material_stock_batches_controller.rb
@@ -3,7 +3,7 @@ class RawMaterialStockBatchesController < ApplicationController
   def new
     @batch = RawMaterialStockBatch.new
     @batch.raw_material_stock_items.build 
-    @item_masters = ItemMaster.where(is_bom: false)
+    @item_masters = ItemMaster.where('"item_masters"."is_bOM" = ?', false)
   end
 
   def show
