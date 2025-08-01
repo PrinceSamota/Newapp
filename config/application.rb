1b9d89a13d7e18386f78257299ae016982b832e0
Navdeep <nnehra@csod.com>
2025-08-01 18:55:11 +0530

    host added

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module MyApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
     config.hosts << "orders.lcareindia.com"
+    config.hosts << "deploing-c1d0485ce657.herokuapp.com/"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
