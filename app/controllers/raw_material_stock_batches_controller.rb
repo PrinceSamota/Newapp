33f4bf23044c862e14051ab04a315d9e65a56ba9
Navdeep <nnehra@csod.com>
2025-08-03 13:48:07 +0530

    STACK-90 | purchase changes

--- app/controllers/raw_material_stock_batches_controller.rb
+++ app/controllers/raw_material_stock_batches_controller.rb
@@ -3,7 +3,7 @@ class RawMaterialStockBatchesController < ApplicationController
   def new
     @batch = RawMaterialStockBatch.new
     @batch.raw_material_stock_items.build 
-    @item_masters = ItemMaster.where('"item_masters"."is_bom" = ?', false)
+    @item_masters = ItemMaster.all
     @selected_supplier_id = params[:supplier_id]
   end
 
