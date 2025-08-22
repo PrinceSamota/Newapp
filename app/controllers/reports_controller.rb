794bf3c6e69f6e2044423a556a641e5f6e260050
Prince_Samota <prince@cogitohub.com>
2025-08-23 03:37:49 +0530

    STAC-153

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,84 +1,104 @@
 class ReportsController < ApplicationController
-    def index
-      order_ids = params[:order_ids].to_s.split(",")
-      @orders = OrderEntry.where(id: order_ids)
+  def index
+    order_ids = params[:order_ids].to_s.split(",")
+    @orders = OrderEntry.where(id: order_ids)
   
-      @report_data = []
+    # collect order numbers for showing in view
+    @selected_order_numbers = @orders.pluck(:order_number) rescue @orders.pluck(:id)
   
-      @orders.each do |order|
-        fg_item = ItemMaster.find_by(sku_id: order.sku_number)
+    sku_hash = {}
   
-        if fg_item
-          bom_as_fg = BillOfMaterial.joins(:finished_good)
-                                    .find_by(finished_goods: { sku_id: fg_item.sku_id })
+    @orders.each do |order|
+      sku_data = expand_bom(order.sku_number, order.qty)
   
-          if bom_as_fg
-            fg_qty = (bom_as_fg.try(:quantity) || 1) * order.qty
-            stock = fg_item.opening_stock || "-"
-            requirement =  stock - fg_qty
-            @report_data << {
-              order_no: order.order_no,
-              fg_sku: order.sku_number,
-              rm_sku: "-",
-              quantity: fg_qty,
-              stock: fg_item.opening_stock || "-",
-              requirement: requirement
-            }
-  
-            bom_as_fg.bom_raw_material_items.each do |rm_item|
-              rm_sku = rm_item.item_master.sku_id
-              rm_qty = rm_item.quantity * order.qty 
-              rm_stock = rm_item.item_master.opening_stock || "-"
-              requirement = rm_stock - rm_qty
-
-              @report_data << {
-                order_no: order.order_no,
-                fg_sku: "-",   
-                rm_sku: rm_sku,
-                quantity: rm_qty,
-              stock: rm_stock,
-              requirement:  requirement
-            
-              }
-            end
-  
-          else
-            bom_as_rm = BomRawMaterialItem.joins(:bill_of_material, :item_master)
-                                          .find_by(item_masters: { sku_id: fg_item.sku_id })
-  
-            if bom_as_rm
-                rm_qty = bom_as_rm.quantity * order.qty 
-                stock = fg_item.opening_stock || "-"
-                requirement = stock - rm_qty
-
-              @report_data << {
-                order_no: order.order_no,
-                fg_sku: "-", 
-                rm_sku: fg_item.sku_id,
-                quantity: rm_qty,
-                stock: stock,
-                requirement: requirement
-                
-              }
-            else
-                stock = fg_item.opening_stock || "-"
-                requirement = stock - qty 
-              @report_data << {
-                order_no: order.order_no,
-                fg_sku: order.sku_number,
-                rm_sku: "-",
-                quantity: rm_qty,
-                stock: stock,
-                requirement: requirement
-              }
-            end
-          end
+      sku_data.values.each do |row|
+        sku = row[:sku]
+        if sku_hash[sku]
+          sku_hash[sku][:quantity] += row[:quantity]
+          sku_hash[sku][:requirement] = sku_hash[sku][:stock] - sku_hash[sku][:quantity]
+        else
+          sku_hash[sku] = row
         end
       end
     end
-
-    def new_order_report
-
+  
+    @report_data = sku_hash.values
+    respond_to do |format|
+      format.html
+      format.xlsx {
+        response.headers['Content-Disposition'] = 'attachment; filename="report.xlsx"'
+      }
     end
   end
   
+  private
+  def expand_bom(sku_id, order_qty, visited = Set.new, parent_bom_qty = 1, top_level: true)
+    return {} if visited.include?(sku_id)
+  
+    item = ItemMaster.find_by(sku_id: sku_id)
+    return {} unless item
+  
+    stock = item.opening_stock || 0
+    rm_final_qty = top_level ? 0 : parent_bom_qty * order_qty
+  
+    result = {
+      sku_id => {
+        sku: sku_id,
+        quantity: rm_final_qty,
+        stock: stock,
+        requirement: stock - rm_final_qty,
+        bom_defined_qty: parent_bom_qty
+      }
+    }
+  
+    visited.add(sku_id)
+  
+    bom = BillOfMaterial.joins(:finished_good).find_by(finished_goods: { sku_id: sku_id })
+    return result unless bom
+  
+    fg_qty = bom.finished_good.quantity.presence || 1
+    fg_final_qty = fg_qty * order_qty
+  
+    result[sku_id][:quantity] += fg_final_qty
+    result[sku_id][:requirement] = result[sku_id][:stock] - result[sku_id][:quantity]
+  
+    bom.bom_raw_material_items.each do |rm|
+      rm_sku = rm.item_master.sku_id
+      rm_qty = rm.quantity * order_qty
+  
+      if BillOfMaterial.joins(:finished_good).exists?(finished_goods: { sku_id: rm_sku })
+        child_result = expand_bom(
+          rm_sku,
+          order_qty,
+          visited.dup,
+          rm.quantity,
+          top_level: false
+        )
+  
+        child_result.each do |sku, data|
+          if result[sku]
+            result[sku][:quantity] += data[:quantity]
+            result[sku][:requirement] = result[sku][:stock] - result[sku][:quantity]
+          else
+            result[sku] = data
+          end
+        end
+      else
+        if result[rm_sku]
+          result[rm_sku][:quantity] += rm_qty
+          result[rm_sku][:requirement] = result[rm_sku][:stock] - result[rm_sku][:quantity]
+        else
+          result[rm_sku] = {
+            sku: rm_sku,
+            quantity: rm_qty,
+            stock: rm.item_master.opening_stock || 0,
+            requirement: (rm.item_master.opening_stock || 0) - rm_qty,
+            bom_defined_qty: rm.quantity
+          }
+        end
+      end
+    end
+  
+    result
+  end
+end
