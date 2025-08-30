09a4e206eab250b8d859c31ae3cd65dafd3b5430
Prince_Samota <prince@cogitohub.com>
2025-08-30 22:26:20 +0530

    Dispatch date bug fix

--- app/controllers/dispatches_controller.rb
+++ app/controllers/dispatches_controller.rb
@@ -42,13 +42,16 @@ class DispatchesController < ApplicationController
   def update
     @dispatch = Dispatch.find(params[:id])
   
+    commit_type = params[:commit_type] 
+  
     ActiveRecord::Base.transaction do
       if @dispatch.update(dispatch_params_update)
         @dispatch.dispatch_items.each do |item|
           order = item.order_entry
           order.update(dispatch_no: @dispatch.d_id) if order.present?
         end
-        if @dispatch.progress == "Dispatched"
+  
+        if commit_type == "full" && @dispatch.progress == "Dispatched"
           PaperTrail.request(controller_info: {
             source_type: "Dispatch",
             source_id: @dispatch.id
@@ -70,12 +73,13 @@ class DispatchesController < ApplicationController
           end
         end
   
-        redirect_to dispatches_path, notice: "Dispatch updated and stock adjusted."
+        redirect_to dispatches_path, notice: "Dispatch updated#{' and stock adjusted' if commit_type == 'full'}."
       else
         render :edit
       end
     end
   end
+  
   def order_details
     @order_entry = OrderEntry.find_by(id: params[:order_entry_id])
     item_master = ItemMaster.find_by(sku_id: @order_entry.sku_number)
