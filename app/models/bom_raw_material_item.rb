f2cf6387f4216375a540d152e4be4ea33f4f294d
Prince_Samota <prince@cogitohub.com>
2025-11-23 18:07:15 +0530

    STAC-210, 203, 163, 176, 177, 194

--- app/models/bom_raw_material_item.rb
+++ app/models/bom_raw_material_item.rb
@@ -1,4 +1,8 @@
 class BomRawMaterialItem < ApplicationRecord
   belongs_to :bill_of_material
   belongs_to :item_master
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :item_master_id, presence: true
+  validates :unit, presence: true
+  validates :sku_id, presence: true
 end
