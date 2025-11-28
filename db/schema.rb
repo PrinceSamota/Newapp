a5077c4618d4339c82c4f873bd9e3c2b9e6166d6
Prince_Samota <prince@cogitohub.com>
2025-11-28 11:27:19 +0530

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_23_121422) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_28_055226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -249,7 +249,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_23_121422) do
     t.integer "complete_description_of_good_id"
     t.integer "hsn_id"
     t.decimal "qty_pcs", precision: 10, scale: 2
-    t.decimal "price", precision: 10, scale: 2
+    t.decimal "price", precision: 10, scale: 4
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["input_id"], name: "index_input_details_on_input_id"
