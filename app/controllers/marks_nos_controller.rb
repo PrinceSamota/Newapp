439714d1b0aa6e9b8c8e9686d81634a167388853
Prince <pankajskumaar26@gmail.com>
2026-04-09 12:52:53 +0530

    search dropdown added

--- app/controllers/marks_nos_controller.rb
+++ app/controllers/marks_nos_controller.rb
@@ -3,24 +3,30 @@ class MarksNosController < ApplicationController
     @marks_no = MarksNo.new(marks_no_params)
     @marks_no.org_id = current_user.org_id
 
+    detail_index = params[:detail_index]
+
     if @marks_no.save
-      render partial: 'inputs/marks_no_dropdown',
-             locals: {
-               detail_index: params[:detail_index],   # controller se view me index pass karo
-               selected_marks_no_id: @marks_no.id,
-               show_form: false,
-               errors: []
-             },
-             formats: [:html]
+      render turbo_stream: turbo_stream.replace(
+        "marks_no_section_#{detail_index}",   # 👈 dynamic id
+        partial: "inputs/marks_no_dropdown",
+        locals: {
+          detail_index: detail_index,
+          selected_marks_no_id: @marks_no.id,  # ✅ auto select
+          show_form: false,
+          errors: []
+        }
+      )
     else
-      render partial: 'inputs/marks_no_dropdown',
-             locals: {
-               detail_index: params[:detail_index],
-               selected_marks_no_id: nil,
-               show_form: true,
-               errors: @marks_no.errors.full_messages
-             },
-             formats: [:html]
+      render turbo_stream: turbo_stream.replace(
+        "marks_no_section_#{detail_index}",
+        partial: "inputs/marks_no_dropdown",
+        locals: {
+          detail_index: detail_index,
+          selected_marks_no_id: nil,
+          show_form: true,
+          errors: @marks_no.errors.full_messages
+        }
+      )
     end
   end
 
