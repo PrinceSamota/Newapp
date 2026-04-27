d2e028889166632fafce83bf3cb9e062118854cc
Gajendra Tomar <tomar.gt@gmail.com>
2026-04-27 16:46:30 +0530

    Label updated

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,5 @@
 WickedPdf.config = {
   exe_path: Rails.env.production? ? '/usr/bin/wkhtmltopdf' : '/usr/bin/wkhtmltopdf',
-  enable_local_file_access: true
+  enable_local_file_access: true,
+  disable_smart_shrinking: true
 }
