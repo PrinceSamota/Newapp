7b41d02a21dbe6b8688c395002412542fa18791b
Gajendra Tomar <tomar.gt@gmail.com>
2026-05-11 15:58:58 +0530

    inputs desc fixed

--- app/models/input_item.rb
+++ app/models/input_item.rb
@@ -1,6 +1,6 @@
 class InputItem < ApplicationRecord
   belongs_to :input, inverse_of: :input_items
-  belongs_to :description_of_good, class_name: 'DescriptionOfGood', foreign_key: 'description_of_goods_id'
+  belongs_to :description_of_good, class_name: 'CompleteDescriptionOfGood', foreign_key: 'description_of_goods_id'
 
   validates :no_of_boxes, :qty_per_box, :net_weight, :gross_weight,
             :length, :width, :height, :order_no, presence: true
